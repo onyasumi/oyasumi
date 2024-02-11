7009472e529736c94510c727ba60c6a8654d525b
onyasumi <boku@kaneki.dev>
2024-02-11 03:35:41 -0500

    Finish rest of UI

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,10 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import ui.MainForm
 import ui.MainFormVM
 
@@ -17,7 +19,7 @@ fun App() {
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(onCloseRequest = ::exitApplication, rememberWindowState(height = 900.dp, width = 1400.dp)) {
         App()
     }
 }
--- src/main/kotlin/ui/MainForm.kt
+++ src/main/kotlin/ui/MainForm.kt
@@ -3,8 +3,10 @@ package ui
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import components.Dropdown
 import components.DropdownItem
 import model.HTTPRequestType
@@ -15,104 +17,182 @@ import model.value
 fun MainForm(vm: MainFormVM) {
 
     Surface {
-        Column {
 
-            Dropdown(
-                items = listOf(
-                    DropdownItem(HTTPRequestType.GET),
-                    DropdownItem(HTTPRequestType.POST),
-                    DropdownItem(HTTPRequestType.PUT),
-                    DropdownItem(HTTPRequestType.PATCH),
-                    DropdownItem(HTTPRequestType.DELETE)
-                ),
-                selectedValue = vm.requestMethod,
-                label = "HTTP Method"
-            )
+        Row {
 
-            var url by remember { mutableStateOf(vm.url) }
-            TextField(value = url,
-                onValueChange = {
-                    vm.url = it
-                    url = it
-                },
-                label = { Text("URL") },
-                singleLine = true,
-                modifier = Modifier.padding(20.dp, 0.dp, 20.dp, 20.dp).fillMaxWidth()
-            )
+            Column(Modifier.weight(1f)) {
 
-            var body by remember { mutableStateOf(vm.body) }
-            TextField(value = body,
-                onValueChange = {
-                    vm.body = it
-                    body = it
-                },
-                label = { Text("Body") },
-                singleLine = false,
-                minLines = 5,
-                modifier = Modifier.padding(20.dp, 0.dp, 20.dp, 20.dp).fillMaxWidth()
-            )
+                Dropdown(
+                    items = listOf(
+                        DropdownItem(HTTPRequestType.GET),
+                        DropdownItem(HTTPRequestType.POST),
+                        DropdownItem(HTTPRequestType.PUT),
+                        DropdownItem(HTTPRequestType.PATCH),
+                        DropdownItem(HTTPRequestType.DELETE)
+                    ),
+                    selectedValue = vm.requestMethod,
+                    label = "HTTP Method"
+                )
 
-            val headerVersionVM by vm.headersVersionLock.collectAsState()
-            var headersVersionLock by remember { mutableIntStateOf(0) }
+                var url by remember { mutableStateOf(vm.url) }
+                TextField(value = url,
+                    onValueChange = {
+                        vm.url = it
+                        url = it
+                    },
+                    label = { Text("URL") },
+                    singleLine = true,
+                    modifier = Modifier.padding(20.dp, 0.dp, 20.dp, 20.dp).fillMaxWidth()
+                )
 
-            val httpHeaders = @Composable {
+                var body by remember { mutableStateOf(vm.body) }
+                TextField(value = body,
+                    onValueChange = {
+                        vm.body = it
+                        body = it
+                    },
+                    label = { Text("Body") },
+                    singleLine = false,
+                    minLines = 5,
+                    modifier = Modifier.padding(20.dp, 0.dp, 20.dp, 20.dp).fillMaxWidth()
+                )
 
-                for(header in vm.headers) {
+                val headerVersionVM by vm.headersVersionLock.collectAsState()
+                var headersVersionLock by remember { mutableIntStateOf(0) }
 
-                    Row(Modifier.padding(20.dp, 0.dp, 20.dp, 20.dp)) {
+                val httpHeaders = @Composable {
 
-                        var key by remember { mutableStateOf(header.key) }
-                        TextField(value = key,
-                            onValueChange = {
-                                header.key = it
-                                key = it
-                            },
-                            label = { Text("Header Key") },
-                            singleLine = true,
-                            modifier = Modifier.weight(1f)
-                        )
+                    for(header in vm.headers) {
 
-                        Spacer(Modifier.width(20.dp))
+                        Row(Modifier.padding(20.dp, 0.dp, 20.dp, 20.dp)) {
 
-                        var value by remember { mutableStateOf(header.value) }
-                        TextField(value = value,
-                            onValueChange = {
-                                header.value = it
-                                value = it
-                            },
-                            label = { Text("Header Value") },
-                            singleLine = true,
-                            modifier = Modifier.weight(1f)
-                        )
+                            var key by remember { mutableStateOf(header.key) }
+                            TextField(value = key,
+                                onValueChange = {
+                                    header.key = it
+                                    key = it
+                                },
+                                label = { Text("Header Key") },
+                                singleLine = true,
+                                modifier = Modifier.weight(1f)
+                            )
 
-                        Spacer(Modifier.width(20.dp))
+                            Spacer(Modifier.width(20.dp))
 
-                        Button(onClick = {vm.deleteHeader(header)},
-                            colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.error),
-                            modifier = Modifier.height(55.dp)) {
-                            Text("Remove")
+                            var value by remember { mutableStateOf(header.value) }
+                            TextField(value = value,
+                                onValueChange = {
+                                    header.value = it
+                                    value = it
+                                },
+                                label = { Text("Header Value") },
+                                singleLine = true,
+                                modifier = Modifier.weight(1f)
+                            )
+
+                            Spacer(Modifier.width(20.dp))
+
+                            Button(onClick = {vm.deleteHeader(header)},
+                                colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.error),
+                                modifier = Modifier.height(55.dp)) {
+                                Text("Remove")
+                            }
+
                         }
 
                     }
 
+
                 }
 
+                if(headersVersionLock != headerVersionVM) {
+                    httpHeaders()
+                    headersVersionLock = headerVersionVM
+                } else {
+                    httpHeaders()
+                    headersVersionLock = headerVersionVM
+                }
+
+                Button(onClick = vm.addHeader, modifier = Modifier.padding(20.dp, 0.dp, 20.dp, 10.dp).fillMaxWidth()) {
+                    Text("Add Custom Header")
+                }
+
+                Row(Modifier.padding(6.dp, 0.dp, 0.dp, 0.dp)) {
+
+                    var isJson by remember { mutableStateOf(false) }
+                    Checkbox(checked = isJson, onCheckedChange = {
+                        isJson = it
+                    })
+
+                    Text(text ="Set Content-type to application/json", fontSize = 20.sp, modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 2.dp).align(Alignment.CenterVertically))
+
+                }
+
+                Row(Modifier.padding(6.dp, 0.dp, 0.dp, 0.dp)) {
+
+                    var allowInsecure by remember { mutableStateOf(false) }
+                    Checkbox(checked = allowInsecure, onCheckedChange = {
+                        allowInsecure = it
+                    })
+
+                    Text(text ="Allow self-signed certificates", fontSize = 20.sp, modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 2.dp).align(Alignment.CenterVertically))
+
+                }
+
+                Row(Modifier.padding(6.dp, 0.dp, 0.dp, 0.dp)) {
+
+                    var isVerbose by remember { mutableStateOf(false) }
+                    Checkbox(checked = isVerbose, onCheckedChange = {
+                        isVerbose = it
+                    })
+
+                    Text(text ="Verbose", fontSize = 20.sp, modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 2.dp).align(Alignment.CenterVertically))
+
+                }
+
+                Spacer(Modifier.weight(1f))
+                Divider(Modifier.padding(15.dp, 10.dp, 15.dp, 5.dp))
+
+                TextField(value = "Sample Text",
+                    onValueChange = {},
+                    readOnly = true,
+                    label = { Text("cURL output") },
+                    singleLine = false,
+                    minLines = 5,
+                    maxLines = 5,
+                    modifier = Modifier.padding(20.dp, 20.dp, 20.dp, 0.dp).fillMaxWidth()
+                )
+
+                Row(Modifier.padding(20.dp)) {
+
+                    Button(onClick = {},
+                        modifier = Modifier.weight(1f)) {
+                        Text("Copy Command")
+                    }
+
+                    Spacer(Modifier.width(20.dp))
+
+                    Button(onClick = {},
+                        modifier = Modifier.weight(1f)) {
+                        Text("Send Request")
+                    }
+
+                }
 
             }
 
-            if(headersVersionLock != headerVersionVM) {
-                httpHeaders()
-                headersVersionLock = headerVersionVM
-            } else {
-                httpHeaders()
-                headersVersionLock = headerVersionVM
-            }
+            Divider(Modifier.width(1.dp).fillMaxHeight())
 
-            Button(onClick = vm.addHeader, modifier = Modifier.padding(20.dp, 0.dp, 20.dp, 20.dp).fillMaxWidth()) {
-                Text("Add Custom Header")
-            }
+            TextField(value = "Sample Text",
+                onValueChange = {},
+                readOnly = true,
+                label = { Text("Response") },
+                singleLine = false,
+                modifier = Modifier.padding(20.dp).weight(1f).fillMaxHeight()
+            )
 
         }
+
     }
 
 }
