7cc39c0c148943e53d5a147ee442e39049d714b8
onyasumi <boku@kaneki.dev>
2024-02-11 06:42:39 -0500

    Fix scroll layout

--- src/main/kotlin/ui/MainForm.kt
+++ src/main/kotlin/ui/MainForm.kt
@@ -1,6 +1,8 @@
 package ui
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -22,159 +24,168 @@ fun MainForm(vm: MainFormVM) {
 
             Column(Modifier.weight(1f)) {
 
-                Dropdown(
-                    items = listOf(
-                        DropdownItem(HTTPRequestType.GET),
-                        DropdownItem(HTTPRequestType.POST),
-                        DropdownItem(HTTPRequestType.PUT),
-                        DropdownItem(HTTPRequestType.PATCH),
-                        DropdownItem(HTTPRequestType.DELETE)
-                    ),
-                    selectedValue = vm.requestMethod,
-                    label = "HTTP Method"
-                )
+                Column(Modifier.weight(1f).verticalScroll(rememberScrollState())) {
 
-                var url by remember { mutableStateOf(vm.url) }
-                TextField(value = url,
-                    onValueChange = {
-                        vm.url = it
-                        url = it
-                    },
-                    label = { Text("URL") },
-                    singleLine = true,
-                    modifier = Modifier.padding(20.dp, 0.dp, 20.dp, 20.dp).fillMaxWidth()
-                )
+                    Dropdown(
+                        items = listOf(
+                            DropdownItem(HTTPRequestType.GET),
+                            DropdownItem(HTTPRequestType.POST),
+                            DropdownItem(HTTPRequestType.PUT),
+                            DropdownItem(HTTPRequestType.PATCH),
+                            DropdownItem(HTTPRequestType.DELETE)
+                        ),
+                        selectedValue = vm.requestMethod,
+                        label = "HTTP Method"
+                    )
 
-                var body by remember { mutableStateOf(vm.body) }
-                TextField(value = body,
-                    onValueChange = {
-                        vm.body = it
-                        body = it
-                    },
-                    label = { Text("Body") },
-                    singleLine = false,
-                    minLines = 5,
-                    modifier = Modifier.padding(20.dp, 0.dp, 20.dp, 20.dp).fillMaxWidth()
-                )
+                    var url by remember { mutableStateOf(vm.url) }
+                    TextField(value = url,
+                        onValueChange = {
+                            vm.url = it
+                            url = it
+                        },
+                        label = { Text("URL") },
+                        singleLine = true,
+                        modifier = Modifier.padding(20.dp, 0.dp, 20.dp, 20.dp).fillMaxWidth()
+                    )
 
-                val headerVersionVM by vm.headersVersionLock.collectAsState()
-                var headersVersionLock by remember { mutableIntStateOf(0) }
+                    var body by remember { mutableStateOf(vm.body) }
+                    TextField(value = body,
+                        onValueChange = {
+                            vm.body = it
+                            body = it
+                        },
+                        label = { Text("Body") },
+                        singleLine = false,
+                        minLines = 5,
+                        modifier = Modifier.padding(20.dp, 0.dp, 20.dp, 20.dp).fillMaxWidth()
+                    )
 
-                val httpHeaders = @Composable {
+                    val headerVersionVM by vm.headersVersionLock.collectAsState()
+                    var headersVersionLock by remember { mutableIntStateOf(0) }
 
-                    for(header in vm.headers) {
+                    val httpHeaders = @Composable {
 
-                        Row(Modifier.padding(20.dp, 0.dp, 20.dp, 20.dp)) {
+                        for(header in vm.headers) {
 
-                            var key by remember { mutableStateOf(header.key) }
-                            TextField(value = key,
-                                onValueChange = {
-                                    header.key = it
-                                    key = it
-                                },
-                                label = { Text("Header Key") },
-                                singleLine = true,
-                                modifier = Modifier.weight(1f)
-                            )
+                            Row(Modifier.padding(20.dp, 0.dp, 20.dp, 20.dp)) {
 
-                            Spacer(Modifier.width(20.dp))
+                                var key by remember { mutableStateOf(header.key) }
+                                TextField(value = key,
+                                    onValueChange = {
+                                        header.key = it
+                                        key = it
+                                    },
+                                    label = { Text("Header Key") },
+                                    singleLine = true,
+                                    modifier = Modifier.weight(1f)
+                                )
 
-                            var value by remember { mutableStateOf(header.value) }
-                            TextField(value = value,
-                                onValueChange = {
-                                    header.value = it
-                                    value = it
-                                },
-                                label = { Text("Header Value") },
-                                singleLine = true,
-                                modifier = Modifier.weight(1f)
-                            )
+                                Spacer(Modifier.width(20.dp))
 
-                            Spacer(Modifier.width(20.dp))
+                                var value by remember { mutableStateOf(header.value) }
+                                TextField(value = value,
+                                    onValueChange = {
+                                        header.value = it
+                                        value = it
+                                    },
+                                    label = { Text("Header Value") },
+                                    singleLine = true,
+                                    modifier = Modifier.weight(1f)
+                                )
 
-                            Button(onClick = {vm.deleteHeader(header)},
-                                colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.error),
-                                modifier = Modifier.height(55.dp)) {
-                                Text("Remove")
+                                Spacer(Modifier.width(20.dp))
+
+                                Button(onClick = {vm.deleteHeader(header)},
+                                    colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.error),
+                                    modifier = Modifier.height(55.dp)) {
+                                    Text("Remove")
+                                }
+
                             }
 
                         }
 
+
                     }
 
-
-                }
-
-                if(headersVersionLock != headerVersionVM) {
-                    httpHeaders()
-                    headersVersionLock = headerVersionVM
-                } else {
-                    httpHeaders()
-                    headersVersionLock = headerVersionVM
-                }
-
-                Button(onClick = vm.addHeader, modifier = Modifier.padding(20.dp, 0.dp, 20.dp, 10.dp).fillMaxWidth()) {
-                    Text("Add Custom Header")
-                }
-
-                Row(Modifier.padding(6.dp, 0.dp, 0.dp, 0.dp)) {
-
-                    var isJson by remember { mutableStateOf(false) }
-                    Checkbox(checked = isJson, onCheckedChange = {
-                        isJson = it
-                    })
-
-                    Text(text ="Set Content-type to application/json", fontSize = 20.sp, modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 2.dp).align(Alignment.CenterVertically))
-
-                }
-
-                Row(Modifier.padding(6.dp, 0.dp, 0.dp, 0.dp)) {
-
-                    var allowInsecure by remember { mutableStateOf(false) }
-                    Checkbox(checked = allowInsecure, onCheckedChange = {
-                        allowInsecure = it
-                    })
-
-                    Text(text ="Allow self-signed certificates", fontSize = 20.sp, modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 2.dp).align(Alignment.CenterVertically))
-
-                }
-
-                Row(Modifier.padding(6.dp, 0.dp, 0.dp, 0.dp)) {
-
-                    var isVerbose by remember { mutableStateOf(false) }
-                    Checkbox(checked = isVerbose, onCheckedChange = {
-                        isVerbose = it
-                    })
-
-                    Text(text ="Verbose", fontSize = 20.sp, modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 2.dp).align(Alignment.CenterVertically))
-
-                }
-
-                Spacer(Modifier.weight(1f))
-                Divider(Modifier.padding(15.dp, 10.dp, 15.dp, 5.dp))
-
-                TextField(value = "Sample Text",
-                    onValueChange = {},
-                    readOnly = true,
-                    label = { Text("cURL output") },
-                    singleLine = false,
-                    minLines = 5,
-                    maxLines = 5,
-                    modifier = Modifier.padding(20.dp, 20.dp, 20.dp, 0.dp).fillMaxWidth()
-                )
-
-                Row(Modifier.padding(20.dp)) {
-
-                    Button(onClick = {},
-                        modifier = Modifier.weight(1f)) {
-                        Text("Copy Command")
+                    if(headersVersionLock != headerVersionVM) {
+                        httpHeaders()
+                        headersVersionLock = headerVersionVM
+                    } else {
+                        httpHeaders()
+                        headersVersionLock = headerVersionVM
                     }
 
-                    Spacer(Modifier.width(20.dp))
+                    Button(onClick = vm.addHeader, modifier = Modifier.padding(20.dp, 0.dp, 20.dp, 10.dp).fillMaxWidth()) {
+                        Text("Add Custom Header")
+                    }
 
-                    Button(onClick = {},
-                        modifier = Modifier.weight(1f)) {
-                        Text("Send Request")
+                    Row(Modifier.padding(6.dp, 0.dp, 0.dp, 0.dp)) {
+
+                        var isJson by remember { mutableStateOf(false) }
+                        Checkbox(checked = isJson, onCheckedChange = {
+                            isJson = it
+                        })
+
+                        Text(text ="Set Content-type to application/json", fontSize = 20.sp, modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 2.dp).align(Alignment.CenterVertically))
+
+                    }
+
+                    Row(Modifier.padding(6.dp, 0.dp, 0.dp, 0.dp)) {
+
+                        var allowInsecure by remember { mutableStateOf(false) }
+                        Checkbox(checked = allowInsecure, onCheckedChange = {
+                            allowInsecure = it
+                        })
+
+                        Text(text ="Allow self-signed certificates", fontSize = 20.sp, modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 2.dp).align(Alignment.CenterVertically))
+
+                    }
+
+                    Row(Modifier.padding(6.dp, 0.dp, 0.dp, 0.dp)) {
+
+                        var isVerbose by remember { mutableStateOf(false) }
+                        Checkbox(checked = isVerbose, onCheckedChange = {
+                            isVerbose = it
+                        })
+
+                        Text(text ="Verbose", fontSize = 20.sp, modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 2.dp).align(Alignment.CenterVertically))
+
+                    }
+
+                    Spacer(Modifier.weight(1f))
+
+                }
+
+                Column(Modifier.height(IntrinsicSize.Min)) {
+
+                    Divider(Modifier.padding(15.dp, 10.dp, 15.dp, 5.dp))
+
+                    TextField(value = "Sample Text",
+                        onValueChange = {},
+                        readOnly = true,
+                        label = { Text("cURL output") },
+                        singleLine = false,
+                        minLines = 5,
+                        maxLines = 5,
+                        modifier = Modifier.padding(20.dp, 20.dp, 20.dp, 0.dp).fillMaxWidth()
+                    )
+
+                    Row(Modifier.padding(20.dp)) {
+
+                        Button(onClick = {},
+                            modifier = Modifier.weight(1f)) {
+                            Text("Copy Command")
+                        }
+
+                        Spacer(Modifier.width(20.dp))
+
+                        Button(onClick = {},
+                            modifier = Modifier.weight(1f)) {
+                            Text("Send Request")
+                        }
+
                     }
 
                 }
